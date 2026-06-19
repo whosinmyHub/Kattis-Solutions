c53aa57e25944e386d6ff4b3caba5a940d659f19
Eman <qundesema@gmail.com>
2026-06-19 11:13:45 -0400

    Create telja.java

---
+++ telja.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class telja {
+    public static void main (String args[]) {
+        int n = new Scanner(System.in).nextInt ();
+        
+        int out = 1;
+      // although "while (n--)" would work in C-like languages, Java doesn't allow this kind of implicit conversion
+        while (n-- > 0) {
+            System.out.println (out++);
+        }
+    }
+}
